9561a5f3c62d55e41f79d590fbef71ce4fbe9cab
binalsasi <binalsasi@gmail.com>
2017-10-03 21:52:08 +0530

    modified MainActivity.java with button callback, added Item placeholder class

---
+++ app/src/main/java/com/dhishna/appdebugger/waiterapp/Item.java
@@ -0,0 +1,12 @@
+package com.dhishna.appdebugger.waiterapp;
+
+/**
+ * Created by dovahkin on 3/10/17.
+ */
+
+public class Item {
+
+    public String name, rating, time, picLink, desc;
+    public int qty;
+    public float price;
+};
--- app/src/main/java/com/dhishna/appdebugger/waiterapp/MainActivity.java
+++ app/src/main/java/com/dhishna/appdebugger/waiterapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.dhishna.appdebugger.waiterapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // uncomment the following statement to automatically redirect to list activity
+        // takeOrder(new View(this));
+    }
+
+    public void takeOrder(View v){
+
+        // change the SelectScreenActivity name if needed to
+        startActivity(new Intent(this, SelectScreenActivity.class));
     }
 }
